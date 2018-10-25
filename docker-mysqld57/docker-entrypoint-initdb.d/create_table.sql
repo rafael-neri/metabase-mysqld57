a61902e16839568bd6af365b1f709ec3c7d19306
szf5 <fku.gpw@gmail.com>
2018-10-25 12:38:22 +0900

    Improved log parser

--- docker-mysqld57/docker-entrypoint-initdb.d/create_table.sql
+++ docker-mysqld57/docker-entrypoint-initdb.d/create_table.sql
@@ -1,5 +1,6 @@
 use  nginx_log;
 -- DROP TABLE IF EXISTS nginx_access_log;
+-- DROP TABLE IF EXISTS nginx_access_log_x;
 CREATE TABLE IF NOT EXISTS nginx_access_log (
     remote_addr VARCHAR(30),
     remote_user VARCHAR(256),
@@ -18,6 +19,22 @@ CREATE TABLE IF NOT EXISTS nginx_access_log (
     upstream_response_time DECIMAL(20 , 3 ),
     upstream_response_length INT,
     upstream_cache_status VARCHAR(256)
+);
+CREATE TABLE IF NOT EXISTS nginx_access_log_x (
+    time_local DATETIME,
+    remote_addr VARCHAR(30),
+    host VARCHAR(256),
+    status SMALLINT,
+    http_method VARCHAR(20),
+    http_req_path VARCHAR(4000),
+    http_req_query VARCHAR(4000),
+    http_req_fragment VARCHAR(1000),
+    http_ver VARCHAR(20),
+    request VARCHAR(4000),
+    http_user_agent VARCHAR(1000),
+    request_time DECIMAL(20 , 3 ),
+    body_bytes_sent INT,
+    server_name VARCHAR(256)
 );
 
 
--- logparser/parse_log.rb
+++ logparser/parse_log.rb
@@ -59,6 +59,48 @@ def generate_sql_for_mysql(data)
   q
 end
 
+def parse_request_line(request_line)
+  r = Hash[[:http_method, :http_req_uri, :http_ver].zip(
+    request_line.to_s.split(' ')
+  )]
+  uri = nil
+  begin
+    uri = URI(r[:http_req_uri].to_s)
+    r.merge!(
+      { :http_req_path => uri.path,
+        :http_req_query => uri.query,
+        :http_req_fragment => uri.fragment })
+  rescue
+  end
+  r
+end
+
+
+def generate_sqlx_for_mysql(data)
+  q = []
+  data.each do |d|
+    rd = parse_request_line(d[:request])
+    s = ["insert into nginx_access_log_x values (#{Array.new(14, '?').join(',')});",
+         d[:time_local],
+         d[:remote_addr],
+         d[:host],
+         to_i_for_sql(d[:status]),
+         rd[:http_method],
+         rd[:http_req_path],
+         rd[:http_req_query],
+         rd[:http_req_fragment],
+         rd[:http_ver],
+         d[:request],
+         d[:http_user_agent],
+         to_f_for_sql(d[:request_time]),
+         to_i_for_sql(d[:body_bytes_sent]),
+         d[:server_name],
+    ]
+    q << ActiveRecord::Base.send(:sanitize_sql_array, s)
+  end
+  q
+end
+
 
 ActiveRecord::Base.establish_connection(
   :adapter => 'mysql2',
@@ -82,6 +124,11 @@ unless skip_log_file_parse? then
       f.puts(sql)
     end
     puts " -> #{file}.sql"
+    sql = generate_sqlx_for_mysql(data)
+    File.open(file + ".sqlx", "w") do |f|
+      f.puts(sql)
+    end
+    puts " -> #{file}.sqlx"
   end
 end
 
